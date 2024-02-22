72f79a502f8962ef154071aaadaf27f72a37a334
ps-syedkhajamoinuddin1 <syekhaja1@publicisgroupe.net>
2024-02-22 08:36:51 +0530

    Predicate functional interface usage

--- src/com/training/Feb20th_Day2/EvenOdd/EvenOdd.java
+++ src/com/training/Feb20th_Day2/EvenOdd/EvenOdd.java
@@ -4,6 +4,8 @@ import java.util.function.Predicate;
 
 public class EvenOdd {
     public static void main(String... args){
+        //Predicate functional interface usage
+        //We can use our own functional interface too
         Predicate<Integer> obj = a -> a%2!=0;
         System.out.println(obj.test(2));
     }
